c0f334909ccb6a4a8a45a81749ab9167b8210d0c
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-23 17:16:33 -0500

    add w7d2 task

--- README.md
+++ README.md
@@ -4,6 +4,39 @@
 
 Install dependencies with `npm install`.
 
+### M1 Mac / macOS Monterey
+
+```
+scheduler % node -v
+v12.22.7
+scheduler % nvm -v
+0.37.2
+scheduler % npm -v
+8.3.1
+```
+
+### Step 1 clone from git https://github.com/lighthouse-labs/scheduler/
+
+### Step 2 Delete package-lock.
+
+### Step 3
+
+```
+npm install
+```
+
+### Step 4
+
+```
+npm run storybook
+```
+
+### Step 5
+
+```
+npm test
+```
+
 ## Running Webpack Development Server
 
 ```sh
---
+++ src/components/DayListItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
+
+export default function DayListItem(props) {
+  console.log(props);
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
+  });
+  function formatSpots(spots) {
+    return `${spots ? spots : 'no'} ${
+      spots !== 1 ? 'spots' : 'spot'
+    } remaining`;
+  }
+  // if(spots){
+  //   return spots: 'no remaining'
+  // }
+  // if(spots !== 1){
+  //   return spots: `${spot} remaining`
+  // }
+  // elseを省略する
+  // まずは、elseを省略した文です。早速書き方の例を示します。
+  // (val == 0)?console.log("valは0です"):console.log("valは0ではないです")
+  // 条件文に「?」を付けて、”True”の場合の処理を記載します。続いて「:」を付けて、”else”の場合の処理を記載します。
+  // シンプルで、一行で書けるので簡単なif分を書く時には便利かと思います。
+  return (
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
+    </li>
+  );
+}

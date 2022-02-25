43fa4afe96c400de488c3d4b84c99f9fa75dbcb2
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-25 16:45:58 -0500

    bug fix error of folder location components/Appointment/index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,79 @@
 // import React from 'react';
 
 import React, { useState } from 'react';
-
 import DayList from './DayList';
 import 'components/Application.scss';
-import InterviewerList from './InterviewerList';
+import Appointment from 'components/Appointment/index.js';
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 3,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Archie Andrews',
+      interviewer: {
+        id: 4,
+        name: 'Cohana Roy',
+        avatar: 'https://i.imgur.com/FK8V841.jpg',
+      },
+    },
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+];
+
+const scheduleList = appointments.map((appointment) => {
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+    />
+  );
+});
+
+const days = [
+  {
+    id: 1,
+    name: 'Monday',
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: 'Tuesday',
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: 'Wednesday',
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: 'Monday',
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: 'Tuesday',
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: 'Wednesday',
-      spots: 0,
-    },
-  ];
   const [day, setDay] = useState('Monday');
 
   return (
@@ -50,7 +100,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        <InterviewerList />
+        {/* <DayList days={days} day={day} setDay={setDay} /> */}
+
+        {scheduleList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );

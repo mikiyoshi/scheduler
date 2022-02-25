b57795c83f7fdd153f51b8242096bc1ea555f7a8
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-25 13:03:11 -0500

    add Appointment contents

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 import './styles.scss';
 
 export default function Appointment(props) {
-  return <article className="appointment">{props.time}</article>;
+  return (
+    <main className="appointment">
+      <article className="appointment">
+        {props.time ? <Header time={props.time} /> : 'No Appointement'}
+      </article>
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </main>
+  );
 }
